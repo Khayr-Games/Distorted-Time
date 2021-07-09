ade0807aa784772c171b101f0a6028e0f5b00eb0
antonygs <antonysarazola@gmail.com>
2021-07-09 20:10:51 -0300

    mejora del salto

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,4 +1,3 @@
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,46 +8,55 @@ public class Player : MonoBehaviour
     [SerializeField] private PlayerInput playerInput;
     [SerializeField] private float velocidad;
     [SerializeField] private float impulso;
+    [SerializeField] private float detectarPiso;
     private Vector2 direccionDeInput;
     Transform jugador;
     Rigidbody2D rb;
     int saltosEchos;
     int limiteDeSaltos = 1;
+    bool tocaSuelo;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
     void Start()
     {
         jugador = GetComponent<Transform>();
         rb = GetComponent<Rigidbody2D>();
 
     }
-    public void OnMoverse (InputValue valor)
+
+    public void OnMoverse(InputValue valor)
     {
-       direccionDeInput = (Vector2)valor.Get();
+        direccionDeInput = (Vector2)valor.Get();
     }
+
     private void Update()
     {
         Mover();
     }
+
+    private void FixedUpdate()
+    {
+        tocaSuelo = Physics2D.OverlapCircle(groundCheck.position, detectarPiso, groundLayer);
+    }
+
     private void Mover()
     {
         jugador.Translate(Vector3.right * this.direccionDeInput.x * Time.deltaTime * this.velocidad, Space.Self);
-        
-     
-        
+
+
+
     }
+
     private void OnSalto()
     {
-        if (saltosEchos < limiteDeSaltos)
+        if (tocaSuelo)
         {
             rb.AddForce(new Vector2(0f, impulso), ForceMode2D.Impulse);
-            saltosEchos++;
+            
         }
-       
+
     }
-    void OnCollisionEnter2D(Collision2D obj)
-    {
-        if (obj.collider.tag == "suelo")
-        {
-            saltosEchos = 0;
-        }
-    }
+
+   
 }
