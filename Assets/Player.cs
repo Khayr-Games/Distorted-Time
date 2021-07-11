5b9859acb72877c11412e27d57b711bcc17e6cb6
antonygs <antonysarazola@gmail.com>
2021-07-11 18:33:16 -0300

    walljump

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -9,14 +9,24 @@ public class Player : MonoBehaviour
     [SerializeField] private float velocidad;
     [SerializeField] private float impulso;
     [SerializeField] private float detectarPiso;
+    [SerializeField] private float detectarPared;
     private Vector2 direccionDeInput;
     Transform jugador;
     Rigidbody2D rb;
-    int saltosEchos;
-    int limiteDeSaltos = 1;
+    bool tocaParedDerecha;
+    bool tocaParedIzquierda;
     bool tocaSuelo;
+    bool tocaSueloP;
+    bool wallJumping;
+    float tocaParedDerechaOIzquierda;
+
+
     public Transform groundCheck;
+    public Transform paredDerechaCheck;
+    public Transform paredIzquierdaCheck;
     public LayerMask groundLayer;
+    public LayerMask paredLayer;
+    
 
     void Start()
     {
@@ -33,30 +43,65 @@ public class Player : MonoBehaviour
     private void Update()
     {
         Mover();
+        WallJumping();
     }
 
     private void FixedUpdate()
     {
         tocaSuelo = Physics2D.OverlapCircle(groundCheck.position, detectarPiso, groundLayer);
+        tocaSueloP = Physics2D.OverlapCircle(groundCheck.position, detectarPiso, paredLayer);
+        tocaParedDerecha = Physics2D.OverlapCircle(paredDerechaCheck.position, detectarPared, paredLayer);
+        tocaParedIzquierda = Physics2D.OverlapCircle(paredIzquierdaCheck.position, detectarPared, paredLayer);
     }
 
     private void Mover()
     {
+       
+        
         jugador.Translate(Vector3.right * this.direccionDeInput.x * Time.deltaTime * this.velocidad, Space.Self);
+         
+    
 
-
-
+ 
     }
 
     private void OnSalto()
     {
-        if (tocaSuelo)
+        if (tocaSuelo || tocaSueloP && !tocaParedDerecha && !tocaParedIzquierda)
         {
             rb.AddForce(new Vector2(0f, impulso), ForceMode2D.Impulse);
             
         }
+       
+        if (tocaParedDerecha && !tocaSuelo)
+        {
+            tocaParedDerechaOIzquierda = -3;
+            wallJumping = true;
+            
+        }
 
+        if (tocaParedIzquierda && !tocaSuelo)
+        {
+            tocaParedDerechaOIzquierda = 3;
+            wallJumping = true;
+        }
+    }
+
+    private void  WallJumping()
+    {
+      if (wallJumping)
+        {
+            rb.velocity = new Vector2(velocidad * tocaParedDerechaOIzquierda, impulso);
+            Invoke("DesactivarWallJumping", 0.1f);
+        }
     }
 
    
+    void DesactivarWallJumping()
+    {
+        rb.constraints = RigidbodyConstraints2D.FreezePositionX;
+        rb.constraints = RigidbodyConstraints2D.None;
+        wallJumping = false;
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
 }
