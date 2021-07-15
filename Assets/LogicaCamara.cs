fd2ec345d7b3d871a72b02f147d2a740f636bf0a
antonygs <antonysarazola@gmail.com>
2021-07-15 18:01:09 -0300

    logica de camara

--- Assets/LogicaCamara.cs
+++ Assets/LogicaCamara.cs
@@ -5,16 +5,38 @@ using UnityEngine;
 public class LogicaCamara : MonoBehaviour
 {
     public Player player;
+    public float maximoY;
+    public float minimoY;
+    private Vector3 posicionRelativa;
     Camera camara;
-
+    Transform posicion;
 
     void Start()
     {
         camara = GetComponent<Camera>();
+        posicion = GetComponent<Transform>();
+        posicionRelativa = transform.position - player.transform.position;
+
     }
 
     void Update()
+    {        
+        MoverCamara();
+        ClampearCamara();
+    }
+
+
+   void MoverCamara()
+    {   
+        if (posicion.position.y > -4 && posicion.position.y < 36)
+        {   
+         transform.position = player.transform.position + posicionRelativa;   
+        }
+              
+    }
+
+    void ClampearCamara()
     {
-        
+         posicion.position = new Vector3(posicion.position.x, Mathf.Clamp(posicion.position.y, minimoY, maximoY), posicion.position.z);   
     }
 }
